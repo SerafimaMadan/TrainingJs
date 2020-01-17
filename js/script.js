e5045d9b53fd4e245c4341c5ad779f4bd3e67889
Serafima <svp1982@mail.ru>
2020-01-17 20:03:19 +0100

    Js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', ( ) => {
 
-const search = document.querySelector('.search');
-const cartBtn = document.getElementById('cart');
-const wishlistBtn = document.getElementById('wishlist');
-const goodsWrapper = document.querySelector('.goods-wrapper');
+const search = document.querySelector('.search'),
+cartBtn = document.getElementById('cart'),
+wishlistBtn = document.getElementById('wishlist'),
+goodsWrapper = document.querySelector('.goods-wrapper'),
+cart = document.querySelector('.cart'),
+category = document.querySelector('.category');
 
 const loading = () => {
 gooodsWrapper.innerHtml = `<div id="spinner"><div class="spinner-loading"><div><div><div></div>
-</div><div><div></div></div><div><div></div></div><div><div></div></div></div></div></div>`
+</div><div><div></div></div><div><div></div></div><div><div></div></div></div></div></div>`;
 };
 
 const getGoods = (handler, filter) => {
@@ -18,34 +20,30 @@ const getGoods = (handler, filter) => {
     .then(handler);
 };
 
-let money=10000;
-const createCardGoods = () => {
-const card = document.createElement('div');
-card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
-card.innerHTML = `<div class="card">
+
+const createCardGoods = (id, title, price, img) => {
+    const card = document.createElement('div');
+    card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
+    card.innerHTML = `<div class="card">
 <div class="card-img-wrapper">
-    <img class="card-img-top" src="img/temp.png" alt="">
-    <button class="card-add-wishlist"></button>
+    <img class="card-img-top" src="${img}" alt="">
+    <button class="card-add-wishlist" data-goods-id="${id}"></button>
 </div>
 <div class="card-body justify-content-between">
-    <a href="#" class="card-title">Имя товара</a>
-    <div class="card-price">${money}₽</div>
+    <a href="#" class="card-title">${title}</a>
+    <div class="card-price">${price}₽</div>
     <div>
-        <button class="card-add-cart">Добавить в корзину</button>
+        <button class="card-add-cart" data-goods-id="${id}">Добавить в корзину</button>
     </div>
 </div>
 </div>`;
 return card;
 };
-goodsWrapper.appendChild(createCardGoods());
-goodsWrapper.appendChild(createCardGoods());
-goodsWrapper.appendChild(createCardGoods());
-
-
+goodsWrapper.appendChild(createCardGoods(1, "Phone", 50000, 'img/temp/Archer.jpg'));
+goodsWrapper.appendChild(createCardGoods(1, "Phone", 50000, 'img/temp/Archer.jpg'));
+goodsWrapper.appendChild(createCardGoods(1, "Phone", 50000, 'img/temp/Archer.jpg'));
 
 const searchGoods = event => {
     event.preventDefault();
 };
-
-
 });
